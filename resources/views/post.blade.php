2f0b7bfbc5e30ae45037cc2d03423f2a1b6e4ca0
kemo-byte <kamalkafi12@gmail.com>
2021-10-20 22:43:51 +0200

    Rout wildcard constraints & Use caching for expensive operations

---
+++ resources/views/post.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>My blog</title>
+    
+
+    
+
+    <link rel="stylesheet" type="text/css" href="/app.css">
+    <!-- <script src="/app.js"></script> -->
+    
+</head>
+<body>
+    
+    <article>
+            <?= $post ?>
+    </article>
+
+    <a href="/"> Go Back </a>
+</body>
+</html>
