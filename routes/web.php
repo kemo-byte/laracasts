d8c4ac32efa40ce9c54b0ba0996defe40d1eb6e2
kemo-byte <kamalkafi12@gmail.com>
2021-10-26 17:44:26 +0200

    queries binding

--- routes/web.php
+++ routes/web.php
@@ -24,7 +24,7 @@ Route::get('/', function () {
         logger($query->sql,$query->bindings); 
     });
     return view('posts',[
-        'posts' => Post::all()
+        'posts' => Post::with('category')->get()
     ]);
 });
 
