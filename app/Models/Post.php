0fa00e412aac84849fb3d72a9f10a1a7792d16a1
kemo-byte <kamalkafi12@gmail.com>
2021-10-26 14:54:10 +0200

    mass assignment add guard and fillable

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -8,5 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     use HasFactory;
-    
+ 
+    protected $fillable = ['title','excerpt','body'];
+
+    // protected $guard = ['id'];
+       
 }
