0056bfb829c92f23cf820f73eb74c8237aa9eda3
kemo-byte <kamalkafi12@gmail.com>
2021-11-29 12:37:16 +0200

    _add-comment-form && newsletter

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -52,31 +52,8 @@
 
 
                 <section class="col-span-8 col-start-5 mt-10 space-y-6">
-                  @auth
-                  <x-panel>
-                    <form action="/posts/{{ $post->slug }}/comments" method="POST">
-                      @csrf
-                      <header class="flex">
-                        <img src="/images/lary-avatar.svg" alt="avatar">
-                        <h2 class="ml-4">Want to participate?</h2>
-                      </header>
+                @include('posts._add-comment-form')
 
-                      <div class="mt-6">
-
-                          <textarea name="body" class="w-full text-sm focus:outline-none focus:ring" cols="30" rows="5" placeholder="Quick, Thing Of Something to Say"></textarea>
-                      </div>
-
-                      <div class="flex justify-end mt-6 pt-6 border-t border-gray-200">
-                        	<button type="submit" class="bg-blue-500 text-white uppercase font-semibold text-xs py-2 px-10 rounded-2xl hover:bg-blue-600">Post</button>
-                      </div>
-                    </form>
-                  </x-panel>
-
-                  @else
-                  <p>
-                      <a href="/register" class="hover:underline" >Register</a> or <a href="/login" class="hover:underline">log in</a> to leave a comment
-                  </p>
-                  @endauth
                   @foreach ($post->comments as $comment)
                     <x-post-comment :comment="$comment" />
                   @endforeach
